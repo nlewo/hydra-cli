2c6e6feea9f3c206b21747d997ea672c54696f40
tobias pflug <tobias.pflug@gmail.com>
2019-07-03 10:30:25 +0200

    Fix indentation

--- default.nix
+++ default.nix
@@ -17,19 +17,19 @@ rec {
   };
   readme = pkgs.runCommand "generate-readme" { buildInputs = [ hydra-cli ]; }
   ''
-  cat ${./README.header.md} > $out
-  echo >> $out
-  echo "## Usage \`hydra-cli\`" >> $out
-  echo >> $out
+    cat ${./README.header.md} > $out
+    echo >> $out
+    echo "## Usage \`hydra-cli\`" >> $out
+    echo >> $out
 
-      hydra-cli help | sed -e 's/^/    /' >> $out
-      echo >> $out
+    hydra-cli help | sed -e 's/^/    /' >> $out
+    echo >> $out
 
-      for s in $(hydra-cli help | sed -n -e '/SUBCOMMANDS/,$p' | grep -v SUBCOMMANDS | grep "^  " | grep -v "^$" | grep -v "^    help" | tr -s " " | cut -d" " -f2); do
+    for s in $(hydra-cli help | sed -n -e '/SUBCOMMANDS/,$p' | grep -v SUBCOMMANDS | grep "^  " | grep -v "^$" | grep -v "^    help" | tr -s " " | cut -d" " -f2); do
       echo "### Command \`hydra-cli $s\`" >> $out
       echo >> $out
       hydra-cli $s --help | sed -e 's/^/    /' >> $out
       echo >> $out
-      done
+    done
   '';
 }
