fda2affe8e5f55b7481d733ad2d09c65e911480b
tobias pflug <tobias.pflug@gmail.com>
2019-07-04 06:16:18 +0200

    Add update-crate-deps script
    Adds a simple scripte to update crate dependencies to the
    nix shell.

--- shell.nix
+++ shell.nix
@@ -6,6 +6,10 @@ let
   rustChannel = pkgs.latest.rustChannels.stable;
   devRust = [ rustChannel.rust ];
   prodRust = [ pkgs.rustc pkgs.cargo ];
+  updateCrateDeps = pkgs.writeScriptBin "update-crate-deps" ''
+    #!/bin/sh
+    ${pkgs.carnix}/bin/carnix generate-nix --src .
+  '';
 in
   pkgs.mkShell {
     buildInputs = [ 
@@ -14,6 +18,7 @@ in
       pkgs.pkg-config
       pkgs.openssl
       pkgs.carnix
+      updateCrateDeps
     ] ++ (if devBuild then devRust else prodRust);
 
   }
