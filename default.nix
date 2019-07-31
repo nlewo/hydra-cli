952324768a1b36ab4a820062726e9b96b17fd1b1
tobias pflug <tobias.pflug@gmail.com>
2019-07-31 14:55:06 +0200

    Drop readme target and test
    This will be reduced with mdsh and `--frozen` invocation

--- default.nix
+++ default.nix
@@ -12,35 +12,7 @@ rec {
     src = sources;
   });
 
-  readme = pkgs.runCommand "generate-readme" { buildInputs = [ hydra-cli ]; }
-  ''
-    cat ${./README.header.md} > $out
-    echo >> $out
-    echo "## Usage \`hydra-cli\`" >> $out
-    echo >> $out
-
-    hydra-cli help | sed -e 's/^/    /' >> $out
-    echo >> $out
-
-    for s in $(hydra-cli help | sed -n -e '/SUBCOMMANDS/,$p' | grep -v SUBCOMMANDS | grep "^  " | grep -v "^$" | grep -v "^    help" | tr -s " " | cut -d" " -f2); do
-      echo "### Command \`hydra-cli $s\`" >> $out
-      echo >> $out
-      hydra-cli $s --help | sed -e 's/^/    /' >> $out
-      echo >> $out
-    done
-  '';
-
   tests = {
-    readme = pkgs.runCommand "test-readme" { } ''
-      if diff -q ${readme} ${./README.md};
-      then
-        echo ok > $out
-      else
-        echo "error: you need to generate the README by running the following command:"
-        echo 'hint : cp $(nix-build -A readme --no-out-link) README.md'
-      fi
-    ''  ;
-
     rustfmt = pkgs.runCommand "test-rustfmt" { buildInputs = [ pkgs.rustfmt ]; }
     ''
       set +e
