6a7522e9435dfacca376c17d4902b4455a8b1c55
tobias pflug <tobias.pflug@gmail.com>
2019-08-12 07:59:15 +0200

    Add comments

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,8 @@
+//! Hydra CI REST API Library
+//!
+//! The HydraClient trait includes operations for querying and creating resources on
+//! a Hydra endpoint.
+
 use crate::hydra::{Eval, Jobset, JobsetConfig, JobsetOverview, Project, ProjectConfig, Search};
 use crate::ops::OpError;
 use reqwest::header::REFERER;
@@ -6,17 +11,20 @@ use serde::de::DeserializeOwned;
 use serde_json::Value;
 use std::collections::HashMap;
 
+/// Stores combination of user and password as required by login
 pub struct Creds {
     pub user: String,
     pub password: String,
 }
 
+/// Errors occuring while talking to the hydra endpoint
 pub enum ClientError {
+    /// Generic error
     Error(String),
+    /// Received data that could not be parsed
     InvalidResponse(String),
 }
 
-// TODO: implement this properly
 impl From<ClientError> for OpError {
     fn from(e: ClientError) -> Self {
         match e {
@@ -33,6 +41,7 @@ impl From<reqwest::Error> for ClientError {
     }
 }
 
+/// HydraRestClient wraps a client with a specific host
 #[derive(Clone)]
 pub struct HydraRestClient<T> {
     host: String,
@@ -46,22 +55,41 @@ impl HydraRestClient<ReqwestClient> {
 }
 
 pub trait HydraClient {
+    /// Authenticates with the server using username and password provided by `Creds`
     fn login(&self, creds: Creds) -> Result<(), ClientError>;
+
+    /// Searches the host for the nix store path `query`
     fn search(&self, query: &str) -> Result<Search, ClientError>;
+
+    /// Retrieves evaluation information for the build specified by `number`
     fn eval(&self, number: i64) -> Result<Eval, ClientError>;
+
+    /// Retrieves the jobset specified by `project` / `jobset`
     fn jobset(&self, project: &str, jobset: &str) -> Result<Jobset, ClientError>;
+
+    /// Creates a jobset called `jobset_name` in the project `project_name` using
+    /// the configuration from `jobset_config`
     fn jobset_create(
         &self,
         project_name: &str,
         jobset_name: &str,
         jobset_config: &JobsetConfig,
     ) -> Result<(), ClientError>;
+
+    /// Retrieves information on all jobsets belonging to `project`
     fn jobset_overview(&self, project: &str) -> Result<Vec<JobsetOverview>, ClientError>;
+
+    /// Retrieves all configured projects
     fn projects(&self) -> Result<Vec<Project>, ClientError>;
+
+    /// Retrieves a project given by `name`
     fn project_create(&self, name: &str) -> Result<(), ClientError>;
+
+    /// Returns the host used
     fn host(&self) -> String;
 }
 
+/// Performs a GET request retrieving a deserializable response
 fn get_json<T: DeserializeOwned>(client: &ReqwestClient, url: &str) -> Result<T, ClientError> {
     let mut res = client
         .get(url)
