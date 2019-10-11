f4a29562899978cd88e7e4b28d4ff064ba05a131
Basile Henry <bjm.henry@gmail.com>
2019-10-11 11:48:08 +0100

    Fix warning

--- src/ops/jobset_eval.rs
+++ src/ops/jobset_eval.rs
@@ -1,4 +1,4 @@
-use crate::hydra::client::{Creds, HydraClient};
+use crate::hydra::client::HydraClient;
 use crate::ops::{ok_msg, OpResult};
 
 pub fn run(client: &dyn HydraClient, project_name: &str, jobset_name: &str) -> OpResult {
